c931b15e87f123381ccf0b63ed3cbaba8782adcd
minddoo <contact@minddoo.github.io>
2026-07-24 20:41:40 +0900

    UI: Update title in checkup tips page to CHECKIT brand

---
+++ update_title.js
@@ -0,0 +1,15 @@
+﻿const fs = require('fs');
+
+let html = fs.readFileSync('C:/Users/pc/Documents/minddoo.github.io/checkup-tips.html', 'utf8');
+
+// Replace the title
+const oldTitle = '💡 건강검진 실무자 강력 추천 18가지 꿀팁';
+const newTitle = '💡 CHECKIT이 알려주는 건강검진 받기 전 꿀팁';
+
+if (html.includes(oldTitle)) {
+    html = html.replace(oldTitle, newTitle);
+    fs.writeFileSync('C:/Users/pc/Documents/minddoo.github.io/checkup-tips.html', html, 'utf8');
+    console.log('Title updated successfully.');
+} else {
+    console.log('Title not found.');
+}
