eff5f956037c57c7ee37317ff52bd1ff2892d18f
minddoo <contact@minddoo.github.io>
2026-07-24 19:13:24 +0900

    Fix search bar: rewrite dictionary.js encoding and add missing firebase scripts

---
+++ add_firebase.js
@@ -0,0 +1,17 @@
+﻿const fs = require('fs');
+
+const files = ['real-estate.html', 'stocks.html', 'finance.html'];
+const firebaseScripts = `
+    <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"></script>
+</head>
+`;
+
+files.forEach(file => {
+    let html = fs.readFileSync(file, 'utf8');
+    if (!html.includes('firebase-app.js')) {
+        html = html.replace('</head>', firebaseScripts);
+        fs.writeFileSync(file, html, 'utf8');
+        console.log(`Added Firebase scripts to ${file}`);
+    }
+});
